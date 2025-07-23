2f85f3f4c3d9addc921c4e7625ba2f5d044d2d16
vishnu <vishnutvalsan77@gmail.com>
2025-07-23 08:40:07 +0530

    second comit

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
-const express= require('express')
-require('dotenv').config()
+const express = require("express");
+require("dotenv").config();
 
-const app= express()
-app.use(express.json())
-const port= 3000;
-const mongoose= require('mongoose')
-const taskRoutes= require('./routes/task')
-app.use('/api/create/task',taskRoutes)
-app.get('/',(req,res)=>{
-    res.send("Welcome")
-})
+const app = express();
+app.use(express.json());
+const port = 3000;
+const mongoose = require("mongoose");
+const taskRoutes = require("./routes/task");
+app.use("/api/v1/task", taskRoutes);
+app.get("/", (req, res) => {
+  res.send("Welcome");
+});
 
-const DbConnect=()=>{
-    try {
-        mongoose.connect(process.env.MONGO_URI)
-        app.listen(port,()=>{
-    console.log(`server runnin on port ${port}`)
-})
-    } catch (error) {
-      console.log(error)  
-    }
-}
+const DbConnect = () => {
+  try {
+    mongoose.connect(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`server runnin on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-DbConnect()
-
+DbConnect();
